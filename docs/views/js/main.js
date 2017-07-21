7d5784d0fc8b75e7b59e0d8c6728214cc21c55e7
ovsundal <osundal83@gmail.com>
2017-07-21 20:03:15 +0200

    feat: move static DOM calls outside of loop for improved scrolling

--- docs/views/js/main.js
+++ docs/views/js/main.js
@@ -517,8 +517,10 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var topScroll = document.body.scrollTop / 1250;
+
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(topScroll + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
